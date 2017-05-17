6f03b8481bd6c7537c76d83c456010693c4b8dca
JordanFreitas <jordanfreitas4@gmail.com>
2017-05-17 11:36:20 -0700

    yoloswag

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* document.addEventListener("DOMContentLoaded", function(event) {
+ document.addEventListener("DOMContentLoaded", function(event) {
     // Create a new input element
     var input = document.createElement("input");
     input.className = "input"; // set the CSS class
@@ -9,71 +9,66 @@
 
     // Assign a click event to your button that:
     document.getElementById("btn1").addEventListener("click", function() {
-        var cont1 = document.getElementById("cont1");
-        if (cont1.style.display == 'none') {
-            document.getElementById("cont1").style.display = "initial";
-                  }
-        else {
-            console.log("here");
-            document.getElementById("cont1").style.display = "none";
-        }
+        document.getElementById("cont1").style.display = "block";
+        document.getElementById("cont2").style.display = "none";
+       document.getElementById("cont3").style.display = "none";
+       document.getElementById("cont4").style.display = "none";
+       document.getElementById("cont5").style.display = "none";
+       document.getElementById("cont6").style.display = "none";
     });
     document.getElementById("btn2").addEventListener("click", function() {
-        // document.getElementById("cont2").style.display = "initial";
-         var cont1 = document.getElementById("cont2");
-        if (cont1.style.display == 'none') {
-            document.getElementById("cont1").style.display = "initial";
-                  }
-        else {
-            console.log("here");
-            document.getElementById("cont1").style.display = "none";
-        }
+         document.getElementById("cont1").style.display = "none";
+        document.getElementById("cont2").style.display = "block";
+       document.getElementById("cont3").style.display = "none";
+       document.getElementById("cont4").style.display = "none";
+       document.getElementById("cont5").style.display = "none";
+       document.getElementById("cont6").style.display = "none";
     });
     document.getElementById("btn3").addEventListener("click", function() {
-        var cont1 = document.getElementById("cont3");
-        if (cont1.style.display == 'none') {
-            document.getElementById("cont1").style.display = "initial";
-                  }
-        else {
-            console.log("here");
-            document.getElementById("cont1").style.display = "none";
-        }
+         document.getElementById("cont1").style.display = "none";
+        document.getElementById("cont2").style.display = "none";
+       document.getElementById("cont3").style.display = "block";
+       document.getElementById("cont4").style.display = "none";
+       document.getElementById("cont5").style.display = "none";
+       document.getElementById("cont6").style.display = "none";
     });
     document.getElementById("btn4").addEventListener("click", function() {
-        var cont1 = document.getElementById("cont4");
-        if (cont1.style.display == 'none') {
-            document.getElementById("cont1").style.display = "initial";
-                  }
-        else {
-            console.log("here");
-            document.getElementById("cont1").style.display = "none";
-        }
+        document.getElementById("cont1").style.display = "none";
+        document.getElementById("cont2").style.display = "none";
+       document.getElementById("cont3").style.display = "none";
+       document.getElementById("cont4").style.display = "block";
+       document.getElementById("cont5").style.display = "none";
+       document.getElementById("cont6").style.display = "none";
     });
     document.getElementById("btn5").addEventListener("click", function() {
-       var cont1 = document.getElementById("cont5");
-        if (cont1.style.display == 'none') {
-            document.getElementById("cont1").style.display = "initial";
-                  }
-        else {
-            console.log("here");
-            document.getElementById("cont1").style.display = "none";
-        }
+        document.getElementById("cont1").style.display = "none";
+        document.getElementById("cont2").style.display = "none";
+       document.getElementById("cont3").style.display = "none";
+       document.getElementById("cont4").style.display = "none";
+       document.getElementById("cont5").style.display = "block";
+       document.getElementById("cont6").style.display = "none";
     });
     document.getElementById("btn6").addEventListener("click", function() {
-         var cont1 = document.getElementById("cont6");
-        if (cont1.style.display == 'none') {
-            document.getElementById("cont1").style.display = "initial";
-                  }
-        else {
-            console.log("here");
-            document.getElementById("cont1").style.display = "none";
-        }
+          document.getElementById("cont1").style.display = "none";
+        document.getElementById("cont2").style.display = "none";
+       document.getElementById("cont3").style.display = "none";
+       document.getElementById("cont4").style.display = "none";
+       document.getElementById("cont5").style.display = "none";
+       document.getElementById("cont6").style.display = "block";
+      
     });
 });
 
 
 
-
+//    var cont1 = document.getElementById("cont6");
+//         if (cont1.style.display == 'none') {
+//             document.getElementById("cont1").style.display = "initial";
+//                   }
+//         else {
+//             console.log("here");
+//             document.getElementById("cont1").style.display = "none";
+//         }
         // // Creates a new paragraph of text from your input element
         // var txt = document.getElementsByTagName('input')[0].value;
         // var paragraph = document.createElement("p");
