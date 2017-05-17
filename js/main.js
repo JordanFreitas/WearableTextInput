fb88148bde12d2063bfe1cbe2f98a284d140b0a2
JordanFreitas <jordanfreitas4@gmail.com>
2017-05-17 11:20:00 -0700

    button implement

--- js/main.js
+++ js/main.js
@@ -8,17 +8,78 @@ document.addEventListener("DOMContentLoaded", function(event) {
     container.appendChild(input);
 
     // Assign a click event to your button that:
-    document.getElementById("my-btn").addEventListener("click", function() {
-
-        // Creates a new paragraph of text from your input element
-        var txt = document.getElementsByTagName('input')[0].value;
-        var paragraph = document.createElement("p");
-        var node = document.createTextNode(txt);
-        paragraph.appendChild(node);
-        document.getElementsByClassName('container')[0].appendChild(paragraph);
-
-        // Clears the text of the input element
-        document.getElementsByTagName('input')[0].value = '';
+    document.getElementById("btn1").addEventListener("click", function() {
+        var cont1 = document.getElementById("cont1");
+        if (cont1.style.display == 'none') {
+            document.getElementById("cont1").style.display = "initial";
+                  }
+        else {
+            console.log("here");
+            document.getElementById("cont1").style.display = "none";
+        }
     });
-
+    document.getElementById("btn2").addEventListener("click", function() {
+        // document.getElementById("cont2").style.display = "initial";
+         var cont1 = document.getElementById("cont2");
+        if (cont1.style.display == 'none') {
+            document.getElementById("cont1").style.display = "initial";
+                  }
+        else {
+            console.log("here");
+            document.getElementById("cont1").style.display = "none";
+        }
+    });
+    document.getElementById("btn3").addEventListener("click", function() {
+        var cont1 = document.getElementById("cont3");
+        if (cont1.style.display == 'none') {
+            document.getElementById("cont1").style.display = "initial";
+                  }
+        else {
+            console.log("here");
+            document.getElementById("cont1").style.display = "none";
+        }
+    });
+    document.getElementById("btn4").addEventListener("click", function() {
+        var cont1 = document.getElementById("cont4");
+        if (cont1.style.display == 'none') {
+            document.getElementById("cont1").style.display = "initial";
+                  }
+        else {
+            console.log("here");
+            document.getElementById("cont1").style.display = "none";
+        }
+    });
+    document.getElementById("btn5").addEventListener("click", function() {
+       var cont1 = document.getElementById("cont5");
+        if (cont1.style.display == 'none') {
+            document.getElementById("cont1").style.display = "initial";
+                  }
+        else {
+            console.log("here");
+            document.getElementById("cont1").style.display = "none";
+        }
+    });
+    document.getElementById("btn6").addEventListener("click", function() {
+         var cont1 = document.getElementById("cont6");
+        if (cont1.style.display == 'none') {
+            document.getElementById("cont1").style.display = "initial";
+                  }
+        else {
+            console.log("here");
+            document.getElementById("cont1").style.display = "none";
+        }
+    });
 });
+
+
+
+
+        // // Creates a new paragraph of text from your input element
+        // var txt = document.getElementsByTagName('input')[0].value;
+        // var paragraph = document.createElement("p");
+        // var node = document.createTextNode(txt);
+        // paragraph.appendChild(node);
+        // document.getElementsByClassName('container')[0].appendChild(paragraph);
+
+        // // Clears the text of the input element
+        // document.getElementsByTagName('input')[0].value = '';
